7c6cb6f5a0feb96d06d533c7863cab79047a4397
YajePanta <yajepanta@gmail.com>
2020-09-22 11:28:22 -0500

    creamos controller y model

--- src/views/index.js
+++ src/views/index.js
@@ -1,7 +1,12 @@
 import viewLogin from './login.js';
 import viewPerfil from './perfil.js';
+import registerView from './register.js';
+import timelineView from './timeline.js';
 
 export const components={
     login:viewLogin,
-    perfil:viewPerfil
+    perfil:viewPerfil,
+    register:registerView,
+    timeline:timelineView,
+
 };
