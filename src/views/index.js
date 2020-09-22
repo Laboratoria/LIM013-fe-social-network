8e407aad3e8334ef5517755cc4911665753ac087
wendy-gs <wendy.gonzales.salcedo@gmail.com>
2020-09-22 11:45:50 -0500

    cambios unidos vistas

--- src/views/index.js
+++ src/views/index.js
@@ -1,7 +1,5 @@
 import viewLogin from './login.js';
 import viewPerfil from './perfil.js';
-import registerView from './register.js';
-import timelineView from './timeline.js';
 
 export const components={
     login:viewLogin,
