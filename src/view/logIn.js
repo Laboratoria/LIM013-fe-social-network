a91bf03314715d409dcc302c85e5e4f9cdad0e75
consuelogoche-1994 <consuelo.goche@gmail.com>
2020-09-22 21:07:52 -0500

    agregando template de logIn y css

--- src/view/logIn.js
+++ src/view/logIn.js
@@ -1,10 +1,9 @@
 export default () => {
   const viewLogIn = `
-  <section class="container-logIn">
   <section class="text-info">
     <p class="text-infoTitle">what is TravelIn?</p>
     <p class="text">Travelin is a social network that unites people who share the same passion for travel.</p>
-    <img src="img/imgLogo1.png">
+    <img src="img/imgLogo.png">
   </section>
   <section class="logIn">
     <div class="box-login">
@@ -23,20 +22,19 @@ export default () => {
         <input type="password" id="password" pattern="[a-zA-Z0-9]{8,20}" placeholder="Password" required />
         </div>
         <button type="submit" class="btn-logIn"><a href="#/home">Log in</a></button>
-      </div>
-      <p class="text">or enter with ...</p>
-      <section class="option">
-        <img src="img/gmail.png" class="gmail">
-        <img src="img/facebook.png" class="facebook">
-      </section>
-      <p class="text">New to TravelIn?</p>
-      <button type="submit" class="newAccount"><a href="#/signUp">Create an account</a></button>
-    </form>
-  </div>
+        <p class="text">or enter with ...</p>
+        <div class="option">
+          <img src="img/gmail.png" class="gmail">
+          <img src="img/facebook.png" class="facebook">
+        </div>
+        <p class="text">New to TravelIn?</p>
+        <button type="submit" class="newAccount"><a href="#/signUp">Create an account</a></button>
+      </form>
+    </div>
   </section>
   `;
   const sectionElement = document.createElement('section');
   sectionElement.innerHTML = viewLogIn;
- 
+  sectionElement.classList.add('container-logIn');
   return sectionElement;
 };
