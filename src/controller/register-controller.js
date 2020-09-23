32067de4d8d69efbd187ff522ae886a8d6080b2a
VeroFlores <veritocalla@gmail.com>
2020-09-23 18:21:44 -0500

    Se añade la función de verificación de correo

--- src/controller/register-controller.js
+++ src/controller/register-controller.js
@@ -1,4 +1,4 @@
-import { signUp, user, verificationEmail } from '../firebase/auth-controller.js';
+import { signUp, user, verificationEmail, createProfileInfo } from '../firebase/auth-controller.js';
 
 export const userRegistration = () => {
   const nameUser = document.querySelector('#nameUser');
@@ -6,6 +6,19 @@ export const userRegistration = () => {
   const passwordLogUp = document.querySelector('#passwordSignUp').value;
 
   signUp(emailLogUp, passwordLogUp).then((result) => {
-console.log(result);
+    createProfileInfo(result.user.uid);
+    verificationEmail().then(() => {
+      // Guardando nombre de usuario en la base de datos
+      const userData = user();
+      updateUserName(userData, nameUser.value);
+  
+      const notification = document.createElement('div');
+      notification.classList.add('notification');
+      notification.textContent = 'Revisa tu correo electrónico para terminar el registro';
+      document.body.appendChild(notification);
+      setTimeout(() => {
+        document.body.removeChild(notification);
+      }, 3000);
+    });
   });
 };
--- src/firebase/auth-controller.js
+++ src/firebase/auth-controller.js
@@ -6,8 +6,8 @@ export const verificationEmail = () => firebase.auth().currentUser.sendEmailVeri
 export const user = () => firebase.auth().currentUser;
 // Profile
 export const createProfileInfo = (id) => {
-    firebase.firestore().collection('users').doc(id).set({
-      aboutMe: 'Cuenta un poco sobre ti',
-      location: 'Ciudad, País',
-    });
-  };
+  firebase.firestore().collection('users').doc(id).set({
+    aboutMe: 'Cuenta un poco sobre ti',
+    location: 'Ciudad, País',
+  });
+};
