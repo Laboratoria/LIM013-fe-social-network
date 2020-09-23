1d0179e86a3a071564e6d8d3f4b35339f9e218ba
consuelogoche-1994 <consuelo.goche@gmail.com>
2020-09-22 19:17:19 -0500

    agregando html a logIn

--- src/view/logIn.js
+++ src/view/logIn.js
@@ -1,24 +1,40 @@
 export default () => {
   const viewLogIn = `
+  <section class="text-info">
+    <p class="text-infoTitle">what is TravelIn?</p>
+    <p class="text">Travelin is a social network that unites people who share the same passion for travel.</p>
+    <img src="img/imgLogo1.png">
+  </section>
   <section class="logIn">
-    <header>
-      <img src="img/TravelIn.png" class="title">
-      <p class="text">¡ welcome to travelin !</p>
-    </header>
-    <form>
-      <input type="email" id="email" placeholder="E-mail" required />
-      <input type="password" id="password" pattern="[a-zA-Z0-9]{8,20}" placeholder="Password" required />
-      <button type="submit" class="btn-logIn"><a href="#/home">Log in</a></button>
+    <div class="box-login">
+      <header>
+        <h1 class="title">TravelIn</h1>
+        <p class="text">¡ welcome to travelin !</p>
+        <p class="text-title">Sign in</p>
+      </header>
+      <form>
+        <div class="input-logIn">
+        <i class="fas fa-envelope"></i>
+        <input type="email" id="email" placeholder="E-mail" required />
+        </div>
+        <div class="input-logIn">
+        <i class="fas fa-lock"></i>
+        <input type="password" id="password" pattern="[a-zA-Z0-9]{8,20}" placeholder="Password" required />
+        </div>
+        <button type="submit" class="btn-logIn"><a href="#/home">Log in</a></button>
+      </div>
+      <p class="text">or enter with ...</p>
+      <section class="option">
+        <img src="img/gmail.png" class="gmail">
+        <img src="img/facebook.png" class="facebook">
+      </section>
+      <p class="text">New to TravelIn?</p>
+      <button type="submit" class="newAccount"><a href="#/signUp">Create an account</a></button>
     </form>
-    <p class="text">or enter with ...</p>
-    <section class="option">
-      <img src="img/gmail.png" class="gmail">
-      <img src="img/facebook.png" class="facebook">
-    </section>
-    <p class="text">New to TravelIn?</p>
-    <button type="submit" class="newAccount"><a href="#/signUp">Create an account</a></button>
+  </div>
   </section>`;
   const sectionElement = document.createElement('section');
   sectionElement.innerHTML = viewLogIn;
+  sectionElement.classList.add('container-logIn');
   return sectionElement;
 };
