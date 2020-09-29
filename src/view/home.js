d2b9d431adc0cf692ee2c6fa9a2458d1ad9c1ce0
consuelogoche-1994 <consuelo.goche@gmail.com>
2020-09-29 10:54:15 -0500

    agregando emailVerified

--- src/view/home.js
+++ src/view/home.js
@@ -1,8 +1,27 @@
 export default () => {
-  const viewHome = `
+  const viewHome = document.createElement('section');
+  viewHome.innerHTML = `
   <p>bienvenidos</p>
+  <p>username</p>
+  <input type="text"> </input>
+  <p>email</p>
+  <input type="text"> </input>
+  <p>fecha de nacimiento</p>
+  <input type="text"> </input>
+  <p>telefono</p>
+  <input type="text"> </input>
+  <p>descripción</p>
+  <input type="text"> </input>
+  <p>País</p>
+  <input type="text"> </input>
+  <p>foto</p>
+  <button class="btn-enviar">Enviar</button>
   `;
-  const sectionElement = document.createElement('section');
-  sectionElement.innerHTML = viewHome;
-  return sectionElement;
+  const db = firebase.firestore();
+  db.collection('datos').add({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815,
+  });
+  return viewHome;
 };
