432d1bbabfef5082e3c9f2f4b32f726da001497d
VeroFlores <veritocalla@gmail.com>
2020-09-29 22:59:36 -0500

    Se esta probando uso de ramas

---
+++ src/controller/profile-controller.js
@@ -0,0 +1,12 @@
+/*export const loadingInfo = () => {
+  const currentUser = userCurrent();
+  getUser(currentUser.uid).then((doc) => {
+    localStorage.setItem('aboutMe', doc.data().aboutMe);
+    localStorage.setItem('location', doc.data().location);
+  });
+  localStorage.setItem('name', currentUser.displayName);
+  const userProfilePhoto = currentUser.photoURL || './img/profile-ico.png';
+  localStorage.setItem('userphoto', userProfilePhoto);
+  localStorage.setItem('userId', currentUser.uid);
+};
+*/
