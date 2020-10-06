e20961493ab346e4d6ff386e63a99a0402d54ad1
Elizabeth Rivera <elizabethriver@gmail.com>
2020-10-06 18:07:48 -0500

    volver a cambios de commit avance post

--- src/controller/router.js
+++ src/controller/router.js
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-cycle */
 import { components } from '../view/index.js';
 // eslint-disable-next-line import/no-unresolved
+import { getAllPosts } from '../firebase/firestore-controller.js';
+import { currentUser } from '../firebase/auth-controller.js';
 
 export const cambioVista = (route) => {
+  const user = currentUser();
   window.location.hash = route;
   const sectionContainer = document.getElementById('container');
   sectionContainer.innerHTML = '';
@@ -24,8 +27,16 @@ export const cambioVista = (route) => {
       routeSelected = sectionContainer.appendChild(components.home());
       break;
     case '#/profile':
-      sectionContainer.innerHTML = '';
-      routeSelected = sectionContainer.appendChild(components.profile());
+      getAllPosts((notes) => {
+        const arrNotes = [];
+        notes.forEach((note) => {
+          if (note.currentUser === user.uid) {
+            arrNotes.push(note);
+          }
+        });
+        sectionContainer.innerHTML = '';
+        routeSelected = sectionContainer.appendChild(components.profile(arrNotes));
+      });
       break;
     default:
       routeSelected = sectionContainer.appendChild(components.notFound());
