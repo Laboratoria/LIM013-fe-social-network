b0b97f1a137a2423417c4258bd492a58e69ffe74
Elizabeth Rivera <elizabethriver@gmail.com>
2020-10-14 11:14:09 -0500

    avance de profile

--- src/view/post.js
+++ src/view/post.js
@@ -1,6 +1,7 @@
 // import { currentUser } from '../firebase/auth-controller.js';
-
 import { deletePost, updatePost } from '../firebase/firestore-controller.js';
+import { currentUser } from '../firebase/auth-controller.js';
+
 
 // const postContentText = (img, post, id) => {
 //   let postContenido = '';
@@ -33,34 +34,58 @@ export const postSection = (Object) => {
   note.classList.add('divPost');
   // const user = currentUser().uid;
   note.innerHTML = ` 
-    <div class="user_post">
+  <div class="user_post">
     <div class="user_photo">
-      <img class="user_img" src="${Object.photo}" alt="">
+      <img class="user_img" src="${Object.photo}" alt="" />
       <h4>${Object.name}</h4>
       <p class="post-time">${Object.time}</p>
     </div>
     <div class="container-name-privacity">
-    <select class="privacy">
-      <option value="0">Publico</option>
-      <option value="1">Privado</option>
-    </select>
+      <select class="privacy">
+        <option value="0">Publico</option>
+        <option value="1">Privado</option>
+      </select>
     </div>
     <div>
-    <p class="input-post" name="" id="inputPost-${Object.id}" type="text" cols="30" rows="10">${Object.post}</p>
-    <button class="hide-btton-post btn-post-save-edit-post" data-id="">Save</button>
-    <button class="hide-btton-post btn-post-cancel-edit-post" data-id="">Cancel</button>
+      <p
+        class="input-post"
+        name=""
+        id="inputPost-${Object.id}"
+        type="text"
+        cols="30"
+        rows="10"
+      >
+        ${Object.post} </p>
+      <button class="hide-btton-post btn-post-save-edit-post" data-id="">
+        Save
+      </button>
+      <button class="hide-btton-post btn-post-cancel-edit-post" data-id="">
+        Cancel
+      </button>
     </div>
-    ${(Object.img !== undefined) ? `<img class="photo_post_img" src="${Object.img}" alt=""/>` : `<img class="hide image-post" src="${Object.img}" alt=""/>`}
+    ${(Object.img !== undefined) ? `<img
+      class="photo_post_img"
+      src="${Object.img}"
+      alt=""
+    />` : `<img class="hide image-post" src="${Object.img}" alt="" />`}
     <div class="container-menu-post" id="containerMenu">
-    <button class="btn-post-edit" data-id="${Object.id}">Editar</button>
-    <button class="btn-post-delete" data-id="${Object.id}">Eliminar</button>
-  </div>
-  <hr class="w3-clear">
+      <button class="btn-post-edit" data-id="${Object.id}">Editar</button>
+      <button class="btn-post-delete" data-id="${Object.id}">Eliminar</button>
+    </div>
+    <hr class="w3-clear" />
     <div id="allComments"></div>
-    <textarea class="input-comment" id="newComment" placeholder="Escribe un comentario"></textarea>
-    <div class='button-section'>
-    <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fas fa-heart"></i> Like</button>
-    <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fas fa-comment"></i> Comment</button>   
+    <textarea
+      class="input-comment"
+      id="newComment"
+      placeholder="Escribe un comentario"
+    ></textarea>
+    <div class="button-section">
+      <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom">
+        <i class="fas fa-heart"></i> Like
+      </button>
+      <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom">
+        <i class="fas fa-comment"></i> Comment
+      </button>
     </div>
   </div>`;
 
