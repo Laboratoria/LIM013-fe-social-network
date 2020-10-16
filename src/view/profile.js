a797737dd6db9b30fc9a40c0ec7c67fe69f42a8c
consuelogoche-1994 <consuelo.goche@gmail.com>
2020-10-16 00:39:01 -0500

    adding getdatapostUser

--- src/view/profile.js
+++ src/view/profile.js
@@ -8,79 +8,87 @@ export default (dataCurrentUser) => {
   const viewProfile = document.createElement('div');
   viewProfile.classList.add('profile-container');
   viewProfile.innerHTML = `
-    <div class="profile-content">
-      <div class="profile-background">
-        <div class="profile-information shadow">
-          <div class="cover-page">
-            <img class="cover-photo" src="${dataCurrentUser.photoCover}">
-          </div>
-          <label id="select-cover" for="select-cover-page">
-            <input type="file" id="select-cover-page" class="hide" accept="image/jpeg, image/png">
-            <span class="edit-cover"><i class="fas fa-camera edit-photo-btn"></i></span>
-          </label>
-          <div class="profile-photo">
-            <img class="photo" src="${dataCurrentUser.photo}">
-          </div>
-          <label id="select-profile" for="select-photo-profile">
-            <input type="file" id="select-photo-profile" class="hide" accept="image/jpeg, image/png">
-            <span class="edit-photo"><i class="fas fa-camera edit-photo-btn"></i></span>
-          </label>
-          <div class="user-information">
-          <span class = "edit-info" id="btn-editProfile"><i class="fas fa-edit"><span class="tooltiptext">Edit information</span></i></span>
-            <h2 class="user-name">${dataCurrentUser.username}</p>
-            <h3>About me</h3>
-            <div class="container-grid">
-              <div><i class="fas fa-envelope"></i><span>${dataCurrentUser.email}</span></div>
-              <div><i class="fas fa-birthday-cake"></i><span>${dataCurrentUser.birthday}</span></div>
-              <div><i class="fas fa-mobile-alt"></i><span>${dataCurrentUser.phone}</span></div>
-              <div><i class="fas fa-map-marker-alt"></i><span>${dataCurrentUser.country}</span></div>
-              <div class="item6"><i class="far fa-id-badge"></i><span>${dataCurrentUser.description}</span>
-                <p></p>
-              </div>
+  <div class="profile-content">
+    <div class="profile-background">
+      <div class="profile-information shadow">
+        <div class="cover-page">
+          <img class="cover-photo" src="${dataCurrentUser.photoCover}">
+        </div>
+        <label id="select-cover" for="select-cover-page">
+          <input type="file" id="select-cover-page" class="hide" accept="image/jpeg, image/png">
+          <span class="edit-cover"><i class="fas fa-camera edit-photo-btn"></i></span>
+        </label>
+        <div class="profile-photo">
+          <img class="photo" src="${dataCurrentUser.photo}">
+        </div>
+        <label id="select-profile" for="select-photo-profile">
+          <input type="file" id="select-photo-profile" class="hide" accept="image/jpeg, image/png">
+          <span class="edit-photo"><i class="fas fa-camera edit-photo-btn"></i></span>
+        </label>
+        <div class="user-information">
+        <span class = "edit-info" id="btn-editProfile"><i class="fas fa-edit"><span class="tooltiptext">Edit information</span></i></span>
+          <h2 class="user-name">${dataCurrentUser.username}</p>
+          <h3>About me</h3>
+          <div class="container-grid">
+            <div><i class="fas fa-envelope"></i><span>${dataCurrentUser.email}</span></div>
+            <div><i class="fas fa-birthday-cake"></i><span>${dataCurrentUser.birthday}</span></div>
+            <div><i class="fas fa-mobile-alt"></i><span>${dataCurrentUser.phone}</span></div>
+            <div><i class="fas fa-map-marker-alt"></i><span>${dataCurrentUser.country}</span></div>
+            <div class="item6"><i class="far fa-id-badge"></i><span>${dataCurrentUser.description}</span>
+              <p></p>
             </div>
           </div>
         </div>
       </div>
-      <div class="user-posts shadow">
-      <p>You have no posts yet!</p>
-      </div>
     </div>
-<section class="modal-container">
-  <section class="modal-settings">
-    <header class="modalHeader">
-      <button class="btn-modalClose"><i class="fa fa-close"></i></button>
-      <h2 class="modalTitle">Edit user profile</h2>
-      <hr>
-    </header>
-    <form class="editProfile">
-      <div class="grupo">
-        <label  for="usernameEdit">User name : </label>
-        <input type="text" id="usernameEdit" value="${dataCurrentUser.username}">
-      </div>
-      <div class="grupo">
-        <label  for="emailEdit">Email : </label>
-        <input type="text" id="emailEdit" disabled value="${dataCurrentUser.email}">
-      </div>
-      <div class="grupo">
-        <label  for="phoneEdit">Phone number : </label>
-        <input type="text" id="phoneEdit" value="${dataCurrentUser.phone}">
-      </div>
-      <div>
-        <label  for="birthdayEdit">Birthday : </label>
-        <input type="date" id="birthdayEdit" value="${dataCurrentUser.birthday}">
-      </div>
-      <div class="grupo">
-        <label  for="countryEdit">Country : </label>
-        <input type="text" id="countryEdit" value="${dataCurrentUser.country}">
-      </div>
-      <div class="grupo">
-        <label  for="countryEdit">Description : </label>
-        <textarea id = "descriptionEdit">${dataCurrentUser.description}</textarea>
-      </div>
-      <button type="submit" class="btn-update">UPDATE</a></button>
-    </form>
+    <div class="user-posts shadow">
+    <p>You have no posts yet!</p>
+    </div>
+  </div>
+  <section class="modal-container">
+    <section class="modal-settings">
+      <header class="modalHeader">
+        <button class="btn-modalClose"><i class="fa fa-close"></i></button>
+        <h2 class="modalTitle">Edit user profile</h2>
+        <hr>
+      </header>
+      <form class="editProfile">
+        <div class="grupo">
+          <label  for="usernameEdit">User name : </label>
+          <input type="text" id="usernameEdit" value="${dataCurrentUser.username}">
+        </div>
+        <div class="grupo">
+          <label  for="emailEdit">Email : </label>
+          <input type="text" id="emailEdit" disabled value="${dataCurrentUser.email}">
+        </div>
+        <div class="grupo">
+          <label  for="phoneEdit">Phone number : </label>
+          <input type="text" id="phoneEdit" value="${dataCurrentUser.phone}">
+        </div>
+        <div>
+          <label  for="birthdayEdit">Birthday : </label>
+          <input type="date" id="birthdayEdit" value="${dataCurrentUser.birthday}">
+        </div>
+        <div class="grupo">
+          <label  for="countryEdit">Country : </label>
+          <input type="text" id="countryEdit" value="${dataCurrentUser.country}">
+        </div>
+        <div class="grupo">
+          <label  for="countryEdit">Description : </label>
+          <textarea id = "descriptionEdit">${dataCurrentUser.description}</textarea>
+        </div>
+        <button type="submit" class="btn-update">UPDATE</a></button>
+      </form>
+    </section>
   </section>
-</section>
+  <section class="modal-progress">
+    <div class="progress">
+      <progress value="0" max="100" id="uploader">0%</progress>
+      <p id="messageProgress">0%</p>
+    </div>
+  </section>
+
+
 
   `;
   /* -----------------change photo profile------------------ */
@@ -88,18 +96,21 @@ export default (dataCurrentUser) => {
   selectPhotoProfile.addEventListener('change', (e) => {
     const file = e.target.files[0];
     const uploadTask = sendImgToStorage(file, 'SN-imgProfile');
+    const messageProgress = viewProfile.querySelector('#messageProgress');
+    const modalProgress = viewProfile.querySelector('.modal-progress');
+    const uploader = viewProfile.querySelector('#uploader');
     uploadTask.on('state_changed', (snapshot) => {
       // Handle progress
       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log(`Upload is ${progress}% done`);
-    }, (error) => {
+      modalProgress.classList.add('showModal');
+      messageProgress.textContent = 'The photo change has been updated successfully';
+      uploader.value = progress;
+    }, () => {
       // Handle unsuccessful uploads
-      console.log(error);
     }, () => {
       // Handle successful uploads on complete
       uploadTask.snapshot.ref.getDownloadURL()
         .then((downloadURL) => {
-          console.log('File available at', downloadURL);
           updatephotoProfile(downloadURL)
             .then(() => window.location.reload());
         });
@@ -110,18 +121,21 @@ export default (dataCurrentUser) => {
   selectCoverPage.addEventListener('change', (e) => {
     const file = e.target.files[0];
     const uploadTask = sendImgToStorage(file, 'SN-imgCover');
+    const messageProgress = viewProfile.querySelector('#messageProgress');
+    const modalProgress = viewProfile.querySelector('.modal-progress');
+    const uploader = viewProfile.querySelector('#uploader');
     uploadTask.on('state_changed', (snapshot) => {
       // Handle progress
       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log(`Upload is ${progress}% done`);
-    }, (error) => {
+      modalProgress.classList.add('showModal');
+      messageProgress.textContent = 'The photo change has been updated successfully';
+      uploader.value = progress;
+    }, () => {
       // Handle unsuccessful uploads
-      console.log(error);
     }, () => {
       // Handle successful uploads on complete
       uploadTask.snapshot.ref.getDownloadURL()
         .then((downloadURL) => {
-          console.log('File available at', downloadURL);
           updatephotoCover(downloadURL)
             .then(() => window.location.reload());
         });
@@ -133,19 +147,19 @@ export default (dataCurrentUser) => {
   const modalContainer = viewProfile.querySelector('.modal-container');
   const btnEditProfile = viewProfile.querySelector('#btn-editProfile');
   btnEditProfile.addEventListener('click' || 'touch', () => {
-    modalContainer.classList.add('showEditProfile');
+    modalContainer.classList.add('showModal');
   });
   /* -----------------close modal edit user profile------------------ */
   const btnModalClose = viewProfile.querySelector('.btn-modalClose');
   btnModalClose.addEventListener('click' || 'touch', (e) => {
     e.preventDefault();
-    modalContainer.classList.remove('showEditProfile');
+    modalContainer.classList.remove('showModal');
     formEditProfile.reset();
   });
   // close modal click outside
   window.addEventListener('click', (e) => {
     if (e.target === modalContainer) {
-      modalContainer.classList.remove('showEditProfile');
+      modalContainer.classList.remove('showModal');
       formEditProfile.reset();
     }
   });
