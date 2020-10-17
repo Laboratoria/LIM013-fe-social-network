752176ed13612b4d4578f59efb5ed0306140048a
Elizabeth Rivera <elizabethriver@gmail.com>
2020-10-17 17:49:05 -0500

    incorporarr post-controller

---
+++ src/controller/post-controller.js
@@ -0,0 +1,22 @@
+import { uploadImgPosting } from '../firebase/storage-controller.js';
+import { crearPostPrueba } from '../firebase/firestore-controller.js';
+
+// TODO makingPost
+export const makingPost = (file, userId, userName, userPhoto) => {
+  const newPost = document.querySelector('#status_input').value;
+  const status = document.querySelector('.privacy').value;
+  const date = new Date().toLocaleString();
+  let imPost = '';
+  if (file) {
+    imPost = localStorage.getItem('image');
+    uploadImgPosting(file, userId);
+  }
+  crearPostPrueba(userId, userName, newPost, imPost, date, status, userPhoto)
+    .then(() => {
+      document.querySelector('#status_input').value = '';
+      const classImg = document.querySelector('.post-new-image');
+      classImg.classList.add('hide');
+      const bttonCancel = document.querySelector('.img-upload-close');
+      bttonCancel.classList.add('hide');
+    });
+};
