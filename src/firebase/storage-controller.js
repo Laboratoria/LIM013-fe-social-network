e4f4776aa929112f590bfe433eef05174394b242
VeroFlores <veritocalla@gmail.com>
2020-10-20 00:16:52 -0500

    Se añade función de cambiar photo de profile haciendo uso de storage

--- src/firebase/storage-controller.js
+++ src/firebase/storage-controller.js
@@ -1,12 +1,12 @@
 // TODO updload photoProfile
 export const uploadPhotoProfile = (file, uid) => {
   const storage = firebase.storage();
-  const refStoragePhoto = storage(`imgPhotoProfile/${uid}/${file.name}`);
+  const refStoragePhoto = storage.ref(`imgPhotoProfile/${uid}/${file.name}`);
   return refStoragePhoto.put(file).then(snapshot => snapshot.ref.getDownloadURL());
 };
 // TODO updload img
 export const uploadImgPosting = (file, uid) => {
   const storage = firebase.storage();
   const refStorageFirebase = storage.ref(`imagePost/${uid}/${file.name}`);
-  refStorageFirebase.put(file);
+  refStorageFirebase.put(file).then(snapshot => snapshot.ref.getDownloadURL());
 };
