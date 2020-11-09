794cb4331a3437de217a28ad1dd66c924e954f94
Diana Sánchez Ordóñez <dianaordonez1998@gmail.com>
2020-11-09 10:10:47 -0500

    pruebas de inicio.js

--- src/main.js
+++ src/main.js
@@ -12,3 +12,13 @@ const init = () => {
 }
 
 window.addEventListener('load', init)
+
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu =  document.querySelector('.btn-menu')
+    if(btn_menu){
+        btn_menu.addEventListener('click', () => {
+            const menu_items =  document.querySelector('.menu-items')
+            menu_items.classList.toggle('show')
+        })
+    }
+})
--- src/views/inicio.js
+++ src/views/inicio.js
@@ -1,9 +1,80 @@
 export default () => {
-    const viewInicio = `
-    <h2 class="text-center">¡Bienvenidos al Home!</h2>
-    `
-    const divElement = document.createElement('div')
-    divElement.innerHTML = viewInicio;
+  const viewInicio = `
+    <section class="post">
+        <main class="card">
+            <div class="card-picture"></div>
+            <div class="card-content">
+                <div class="card-header">
+                    <div class="profile-pic"></div>
+                    <div class="detail">
+                        <p class="name">Miranda</p>
+                        <p class="posted">2 hours ago</p>
+                    </div>
+                </div>
+                <div class="description">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing
+                    elit. Exercitationem optio omnis animi alias voluptas
+                    unde cum obcaecati, suscipit laboriosam earum.
+                </div>
+                <div class="tags">
+                    <span>#party</span>
+                    <span>#colorful</span>
+                </div>
+                <div class="footer">
+                    <div class="like">
+                        <i class="fas fa-heart"></i>
+                        <span>12k</span>
+                    </div>
+                    <div class="comment">
+                        <i class="fas fa-comment"></i>
+                        <span>12k</span>
+                    </div>
+                    <div class="share">
+                        <i class="fas fa-share"></i>
+                        <span>12k</span>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <main class="card">
+            <div class="card-picture"></div>
+            <div class="card-content">
+                <div class="card-header">
+                    <div class="profile-pic"></div>
+                    <div class="detail">
+                        <p class="name">Miranda</p>
+                        <p class="posted">2 hours ago</p>
+                    </div>
+                </div>
+                <div class="description">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing
+                    elit. Exercitationem optio omnis animi alias voluptas
+                    unde cum obcaecati, suscipit laboriosam earum.
+                </div>
+                <div class="tags">
+                    <span>#party</span>
+                    <span>#colorful</span>
+                </div>
+                <div class="footer">
+                    <div class="like">
+                        <i class="fas fa-heart"></i>
+                        <span>12k</span>
+                    </div>
+                    <div class="comment">
+                        <i class="fas fa-comment"></i>
+                        <span>12k</span>
+                    </div>
+                    <div class="share">
+                        <i class="fas fa-share"></i>
+                        <span>12k</span>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </section>
+    `;
+  const divElement = document.createElement("div");
+  divElement.innerHTML = viewInicio;
 
-    return divElement
-}
+  return divElement;
+};
