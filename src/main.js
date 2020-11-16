2c16a777ca8b5078e1d4f7d68a3564bf33111773
Egda Barrios <egdabarrios@gmail.com>
2020-11-16 11:17:05 -0500

    configuracion inicial

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 // Este es el punto de entrada de tu aplicacion
-
+/*
 import { myFunction } from './lib/index.js';
 
 myFunction();
+*/
+
+//-------------------------------------------------------------------------------------
+
+const init = () => {
+    window.addEventListener('hashchange', () => console.log(window.location.hash))
+}
+
+window.addEventListener('load', init);
