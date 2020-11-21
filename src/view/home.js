e4109e74a4a597a4a9658eddc574ea44a9ed12ff
Egda Barrios <egdabarrios@gmail.com>
2020-11-21 09:15:46 -0500

    añadiendo funcionalidad de display a formularios log in y sign in

--- src/view/home.js
+++ src/view/home.js
@@ -61,25 +61,42 @@ export const mainView = () => {
       <section class= "home_form">
         <section class= "emptySpace">
         </section>
-        <section class= "signInOptionForm">
+        <section class= "logInOptionForm" id="logInOptionForm">
           <table class= "formOptions">
             <tr>
-              <th class= "signInOptionCell">
-                
-              </th>
+              <th class= "logIn">LOG IN</th>
+              <th class= "signInOptionCell"><button type="button" class="signInOptionButton" id="signInOptionButton">SIGN IN</button></th>
+            </tr>
+          </table>
+          <form name= "logInForm" id= "logInForm" class= "initForm">
+            <section class= "form-group">
+              <input type="text" placeholder="Email" id="logInEmailInput" name="logInEmailInput" class="input">
+            </section>
+            <section class= "form-group">
+              <input type="password" placeholder="Password" id="logInPasswordInput" name="logInPasswordInput" class="input">
+            </section>
+            <button type="button" class="logInButton" id="logInButton">LOG IN</button>
+            <p>or</p>
+            <button type="button" class="logInWithFacebookButton">LOG IN WITH FACEBOOK</button>
+          </form>
+        </section>
+        <section class= "signInOptionForm" id="signInOptionForm">
+          <table class= "formOptions">
+            <tr>
+              <th class= "logInOptionCell"><button type="button" class="logInOptionButton" id="logInOptionButton">LOG IN</button></th>
               <th class= "signIn">SIGN IN</th>
             </tr>
           </table>
           <form name= "signInForm" id= "signInForm" class= "initForm">
             <section class= "form-group">
-              <input type="text" placeholder="Email" id="signInEmailInput" name="signInEmailInput" class="input" required>
+              <input type="text" placeholder="Email" id="signInEmailInput" name="signInEmailInput" class="input">
             </section>
             <section class= "form-group">
-              <input type="password" placeholder="Password" id="signInPasswordInput" name="signInPasswordInput" class="input" required>
+            <input type="password" placeholder="Password" id="signInPasswordInput" name="signInPasswordInput" class="input">
             </section>
             <button type="button" class="signInButton" id="signInButton">SIGN IN</button>
             <p>or</p>
-            <button type="button" class="logInWithFacebookButton">SIGN IN WITH FACEBOOK</button>
+            <button type="button" class="signInWithFacebookButton">SIGN IN WITH FACEBOOK</button>
           </form>
         </section>
         <p class="logo"><b>COS</b>play</p>
@@ -91,8 +108,29 @@ export const mainView = () => {
   // --- Insertando el template en la interfaz
   sectionElement.innerHTML = template;
 
+  // --- Mostrando solo el formulario de LOG IN al cargar la página
+  window.onload=()=> {
+    document.getElementById("signInOptionForm").style.display = "none"; 
+    document.getElementById("logInOptionForm").style.display = "block";
+  }
+
+  // --- Selecionando las opciones de formulario LOG IN/SIGN IN
+  const signInOpBtn = sectionElement.querySelector('#signInOptionButton');
+  
+  signInOpBtn.addEventListener('click', () => {
+    document.getElementById("logInOptionForm").style.display = "none"; 
+    document.getElementById("signInOptionForm").style.display = "block";
+  })
+
+  const logInOpBtn = sectionElement.querySelector('#logInOptionButton');
+  
+  logInOpBtn.addEventListener('click', () => {
+    document.getElementById("logInOptionForm").style.display = "block"; 
+    document.getElementById("signInOptionForm").style.display = "none";
+  })
+  
   // --- Funcionalidades para capturar lo escrito en el input y poner evento de click a el boton de "Sign in"
-  const button = sectionElement.querySelector('button');
+  const button = sectionElement.querySelector('#signInButton');
   
   button.addEventListener('click', () => {
     const signInEmail = sectionElement.querySelector('#signInEmailInput').value;
@@ -110,13 +148,3 @@ export const mainView = () => {
 
   return sectionElement;
 };
-
-/*
-<form name= "signInForm" class= "initForm">
-  <input type="text" placeholder="Email" id="signInEmailInput" name="signInEmailInput" class="input">
-  <input type="password" placeholder="Password" id="signInPasswordInput" name="signInPasswordInput" class="input">
-  <button type="button" class="signInButton" id="signInButton">SIGN IN</button>
-  <p>or</p>
-  <button type="button" class="logInWithFacebookButton">SIGN IN WITH FACEBOOK</button>
-</form>
-*/
