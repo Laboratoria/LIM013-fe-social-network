9b7ddaaf904b89439321e13180e747d527a398b8
Egda Barrios <egdabarrios@gmail.com>
2020-11-28 10:46:56 -0500

    añadiendo funcionalidades para segunda vista

--- src/view-controler/route.js
+++ src/view-controler/route.js
@@ -1,9 +1,25 @@
+import { components } from '../view/index.js';
+
+//---------------------------------------------------------------------
+
 /* eslint-disable no-console */
 // Creando controlador para asociar el cambio de la ruta con las vistas
 // Fucion para asociar vistas , que recibe la ruta que vamos a traer de
 
-const changeView = (route) => {
+export const changeView = (route) => {
+  const container = document.getElementById('container');
+  container.innerHTML = '';
+  switch (route) {
+    case '#/':
+      { return container.appendChild(components.signAndLog()); }
+    case '#/home':
+      { return container.appendChild(components.home()); }
+    case '#/profile': 
+      { return container.appendChild(components.profile()); }
+    default:
+      break;
+  }
   console.log(route);
 };
 
-export { changeView };
+//export { changeView };
---
+++ src/view/index.js
@@ -0,0 +1,7 @@
+import SignAndLog from './signAndLog.js';
+import Home from './home.js';
+
+export const components = {
+    signAndLog: SignAndLog,
+    home: Home
+}
