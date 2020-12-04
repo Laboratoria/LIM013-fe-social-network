bd336d6fe38cb53bc54c91493e8e9bc2b2b53c3d
Egda Barrios <egdabarrios@gmail.com>
2020-12-04 09:05:36 -0500

    ajustes

--- src/view/home.js
+++ src/view/home.js
@@ -1,28 +1,26 @@
 export default () => {
   const homeView = `
-    <section class="wrapper"
-      <header></header>
-      <main class="mainHome">
-        <section class="newPostContainer">
-          <section class="postFeed">
-            <section class="newPost">
-              <form class="newPostForm">
-                <section class="form-groupPost">
-                  <input type="text" placeholder="What have you been up to lately?" id="newPostText" name="newPostText" class="newPostInputText" ><br>
-                  <hr class="separator">
+    <header></header>
+    <main class="mainHome">
+      <section class="newPostContainer">
+        <section class="postFeed">
+          <section class="newPost">
+            <form class="newPostForm">
+              <section class="form-groupPost">
+                <input type="text" placeholder="What have you been up to lately?" id="newPostText" name="newPostText" class="newPostInputText" ><br>
+                <hr class="separator">
+              </section>
+              <section class="form-groupPost">
+                <section class="callToAction">
+                  <button type="button" class="postButton" id="postButton">POST</button>
                 </section>
-                <section class="form-groupPost">
-                  <section class="callToAction">
-                    <button type="button" class="postButton" id="postButton">POST</button>
-                  </section>
-                </section>
-              </form>
-            </section>
+              </section>
+            </form>
           </section>
         </section>
-      </main>
-      <aside></aside>
-    </section>
+      </section>
+    </main>
+    <aside></aside>
   `;
   // Insertando el template en la interfaz
   const sectionElement = document.createElement('section');
