fb0041d485676ccb27d520ed494a83d3b54b8f01
Egda Barrios <egdabarrios@gmail.com>
2020-12-06 18:09:42 -0500

    error404View

--- src/view/error404.js
+++ src/view/error404.js
@@ -1,9 +1,20 @@
 export default () => {
   const error404View = `
-    <h2>404</h2>
-    <h1>Pagina no encontrada</H1>
-    <p>El archivo especificado no se encontro en este sitio web . 
-    Por favor compruebe la URL para errores y vuelva a intentarlo</p>
+    <section class="error404Wrapper">
+      <main class="error404Main">
+        <section class="errorText">
+          <p class="reallyBigNumber">404</p>
+          <p class="warning">Page not found</p>
+          <p class="supportText">Don't worry, let's find you a better place to go.</p>
+          <button class="returnButton" id="goHome"><span class="footerBold">COS</span>play Home</button>
+        </section>
+        <section class="themeImage">
+        </section>
+      </main>
+      <footer class="footer">@<span class="footerBold">COS</span>play |
+        <span class="footerBold">Developed by</span>: Team 4 | Social Network |Laboratoria
+      </footer>
+    <section>
     `;
   
   // Insertando el template en la interfaz
@@ -12,5 +23,11 @@ export default () => {
   sectionElement.setAttribute('id', 'message');
   sectionElement.innerHTML = error404View;
 
+  const goHomeBtn = sectionElement.querySelector('#goHome');
+
+  goHomeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    return window.location.hash = '#/home';
+  });
   return sectionElement;
 };
